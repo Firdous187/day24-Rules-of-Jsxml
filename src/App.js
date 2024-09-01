951a0abfe9543a55e7be63a8710935720592de4c
Firdous187 <Shaik.firdous0011@gmail.com>
2024-09-01 10:26:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <div>
+    <h1 className="classh1">Jsxml Rule</h1>
+    <h2>JSX stands for JavaScript XML.
+
+    JSX allows us to write HTML in React.
+    
+    JSX makes it easier to write and add HTML in React.</h2>
+    <p>html we have <br></br>*class <br></br> *for</p>
+    <p> So In Jsxml we have already have class in JSXML so it has conflict so it can changes from className</p>
+  
+    <p>In Jsxml</p>
+    <p>*For changes from htmlfor</p>
+    <p>Example on for attritubes</p>
+
+    <div>
+    <form>
+    <caption>Application Form </caption>
+    <div>
+    <label Htmlfor="firstName" >First Name</label>
+    <input id="firstName"></input>
+    <label Htmlfor="lastName" class="LName">last Name</label>
+    <input id="lastName"></input>
+    </div>
+    <div>
+    <label Htmlfor="eng">English marks</label>
+    <input id="eng"></input>
+    </div>
+    <div>
+    <label Htmlfor="maths">Maths marks</label>
+    <input id="maths"></input>
+    </div>
+    <div>
+    <label Htmlfor="soc">Social marks</label>
+    <input id="soc"></input>
+    </div>
+    <div>
+    <label Htmlfor="sci">Science marks</label>
+    <input id="sci"></input>
+    </div>
+    <div>
+    <label Htmlfor="phy">Physics marks</label>
+    <input id="phy"></input>
+    </div>
+    <div>
+    <label Htmlfor="hin">Hindi marks</label>
+    <input id="hin"></input>
+    </div>
+    <div>
+    <label Htmlfor="tel">Telugu marks</label>
+    <input id="tel"></input>
+    </div>
+    <button>Submit</button>
+    </form>
+    </div>
+    <p>*class is changes from className</p>
+    <p>We dont use tag instead of using link tag we use <mark>Import tag</mark> </p>
+    <p> Every tag has a ending tag</p>
+    <p>In jsxml it accepts only one tag its accept so many child tag</p>
+    </div>
+    </div>
+  );
+}
+
+export default App;
